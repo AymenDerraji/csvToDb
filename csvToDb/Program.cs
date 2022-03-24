e105422571c3d897b31a8eed4dca68a1eb4c576b
peydevgit <paiman1991@gmail.com>
2022-03-24 23:04:41 +0100

    Kommentarer

--- csvToDb/Program.cs
+++ csvToDb/Program.cs
@@ -12,40 +12,33 @@ namespace csvToDb
             // En lista av klassen Survey
             List<Survey> SurveyList = new List<Survey>();
 
-            ////// CSV - TextFieldParser
+            // CSV - TextFieldParser 
             using (var reader = new TextFieldParser("SurveyForm.csv"))
             {
-                reader.SetDelimiters(new string[] { "," });
+                reader.SetDelimiters(new string[] { "," }); // vi gör avgränsningar mellan varje komma tecken(obs kommatecken i " , " ignoreras.
 
                 reader.ReadLine(); // Läs och glöm bort första raden
 
+                // while loop så länge den inte har läst igenom all data text i csv filen.
                 while (!reader.EndOfData)
-                {
-                    try
+                {   
+                    try // En try catch för att hantera felsökning lättare ifall det finns någon data text som inte går omvandla till rätt objekt av klassen.
                     {
-                        string[] columns = reader.ReadFields();
-                        SurveyList.Add(new Survey(int.Parse(columns[1]), columns[2], columns[3], int.Parse(columns[4])));
+                        string[] columns = reader.ReadFields(); // Gör om raderna till arrayer.
+                        SurveyList.Add(new Survey(int.Parse(columns[1]), columns[2], columns[3], int.Parse(columns[4]))); // Lägger varje index av kolumerna i listan
                     }
-                    catch (Exception)
+                    catch (Exception error) // Kommer hit ifall den hittar ett fel!
                     {
-                        Console.WriteLine("line x is invalid. skipping");
+                        Console.WriteLine($"Hoppsan! {error} hände när den försökte läsa en rad i filen."); // felmedelandet
                     }
                 }
             }
 
-            ////// Exempel på forloop in i listan
-            //foreach (Survey surveys in SurveyList)
-            //{
-            //    Console.WriteLine($"{surveys.MeatWeeks}, {surveys.KindOfFood}, {surveys.HomeOrRestaurant}, {surveys.LunchTime}");
-            //}
-            //    Console.WriteLine("Done");
-
-            //// Läsa in objekten till databasen
 
             //// --------------
             //// NoSQL-databaser!
 
-            //// Anslut till databasen (i filen MyData.db)
+            //// Skapa / Anslut till databasen (MyData.db)
             using (var db = new LiteDatabase("MyData.db"))
             {
                 // All data är organiserad i "collections"
@@ -53,11 +46,10 @@ namespace csvToDb
 
                 // Spara undan min variabel ovanför
                 surveyDB.DeleteAll(); // raderar allt Sen raden under lägger vi om allt på nytt så det inte blir dubletter, det kan finnas en bättre lösning
-                // serialiserar objektet till JSON och sparar undan det
-                surveyDB.Insert(SurveyList); 
-                // som en del av MyData.db
+                surveyDB.Insert(SurveyList); // Vi lägger listan in i databasen.
+                
                 // Fråga 1, hur många har svarat på formuläret
-                var answer = surveyDB.Count();
+                var answer = surveyDB.Count(); 
                 Console.WriteLine(answer);
                 // Fråga 2, medelvärdet av kött konsumtionen
 
@@ -66,11 +58,12 @@ namespace csvToDb
                 // Fråga 4, hur många av alla som svarat på formuläret äter kött
 
                 // Fråga 5, hur många som svarade på formuläret äter oftast hemma
+
+                // Fråga 6, hur många som äter vid klockan 12.
                 var result = surveyDB.Query().Where(x => x.LunchTime == 12).Count();
-                // la till en test så vi ser att data finns i data basen och hämtar alla som har lunchTid vid 12
-                Console.WriteLine($"Antalet personer som äter lunch vid 12 är {result} stycken"); // vi ser här att svaret blir 9 av 13 som har lunch vid 12.
-                /* medelvärdet av kött konsumtion, hur många av alla som svarat på formuläret äter kött, populäraste typen av mat, hur många har svarat på formuläret,
-               hur många som svarade på formuläret äter helre hemma */
+                
+                Console.WriteLine($"Antalet personer som äter lunch vid 12 är {result} stycken");
+
             }
         }
     }
--- csvToDb/Survey.cs
+++ csvToDb/Survey.cs
@@ -3,11 +3,13 @@ namespace csvToDb
 {
         internal class Survey
         {
+            // Skapar fields så vi kan senare använda i konstruktorn för att skapa objekt för datan i csv filen.
             public int MeatWeeks { get; set; }
             public string KindOfFood { get; set; }
             public string HomeOrRestaurant { get; set; }
             public int LunchTime { get; set; }
 
+            // En konstruktor för klassen.
             public Survey(int weeks, string foodkind, string homerestaurant, int lunchtime)
             {
                 MeatWeeks = weeks;
