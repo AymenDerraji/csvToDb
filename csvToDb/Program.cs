f3002f68cd3aeb0a1bc6cc13718f2d07099401f3
Uroise <simon.hultqvist@gmail.com>
2022-03-24 20:28:49 +0100

    Frågor

--- csvToDb/Program.cs
+++ csvToDb/Program.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using LiteDB;
+﻿using LiteDB;
 using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
 
 namespace csvToDb
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // En lista av klassen Survey
             List<Survey> SurveyList = new List<Survey>();
-
 
             ////// CSV - TextFieldParser
             using (var reader = new TextFieldParser("SurveyForm.csv"))
@@ -26,15 +25,12 @@ namespace csvToDb
                     {
                         string[] columns = reader.ReadFields();
                         SurveyList.Add(new Survey(int.Parse(columns[1]), columns[2], columns[3], int.Parse(columns[4])));
-
                     }
                     catch (Exception)
                     {
                         Console.WriteLine("line x is invalid. skipping");
                     }
-
                 }
-
             }
 
             ////// Exempel på forloop in i listan
@@ -44,10 +40,7 @@ namespace csvToDb
             //}
             //    Console.WriteLine("Done");
 
-
-
             //// Läsa in objekten till databasen
-
 
             //// --------------
             //// NoSQL-databaser!
@@ -58,16 +51,25 @@ namespace csvToDb
                 // All data är organiserad i "collections"
                 var surveyDB = db.GetCollection<Survey>("SurveyForm");
 
-
                 // Spara undan min variabel ovanför
                 surveyDB.DeleteAll(); // raderar allt Sen raden under lägger vi om allt på nytt så det inte blir dubletter, det kan finnas en bättre lösning
-                surveyDB.Insert(SurveyList); // serialiserar objektet till JSON och sparar undan det 
-                                             // som en del av MyData.db
-                                             // Fråga 1, hur många har svarat på formuläret 
+                // serialiserar objektet till JSON och sparar undan det
+                surveyDB.Insert(SurveyList); 
+                // som en del av MyData.db
+                // Fråga 1, hur många har svarat på formuläret
+                var answer = surveyDB.Count();
+                Console.WriteLine(answer);
+                // Fråga 2, medelvärdet av kött konsumtionen
+
+                // Fråga 3, Populäraste typen av mat
+
+                // Fråga 4, hur många av alla som svarat på formuläret äter kött
+
+                // Fråga 5, hur många som svarade på formuläret äter oftast hemma
                 var result = surveyDB.Query().Where(x => x.LunchTime == 12).Count();
                 // la till en test så vi ser att data finns i data basen och hämtar alla som har lunchTid vid 12
                 Console.WriteLine($"Antalet personer som äter lunch vid 12 är {result} stycken"); // vi ser här att svaret blir 9 av 13 som har lunch vid 12.
-                /* medelvärdet av kött konsumtion, hur många av alla som svarat på formuläret äter kött, populäraste typen av mat, hur många har svarat på formuläret, 
+                /* medelvärdet av kött konsumtion, hur många av alla som svarat på formuläret äter kött, populäraste typen av mat, hur många har svarat på formuläret,
                hur många som svarade på formuläret äter helre hemma */
             }
         }
